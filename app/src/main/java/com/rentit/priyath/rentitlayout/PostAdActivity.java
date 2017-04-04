d3513dda02255c1b5e5efca5c3ea668c924d45d9
Priyath Saji <Priyath Saji>
2017-04-04 09:48:05 +0530

    added location services

--- app/src/main/java/com/rentit/priyath/rentitlayout/PostAdActivity.java
+++ app/src/main/java/com/rentit/priyath/rentitlayout/PostAdActivity.java
@@ -1,7 +1,11 @@
 package com.rentit.priyath.rentitlayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,10 +17,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+
+/*TODO
+
+has to call the location activity to get location then add using that location to post the data
+save the data collected to a class object and save it in storage using file stream
+
+*/
 public class PostAdActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    EditText subinfo1;
+    EditText subinfo2;
+    EditText subinfo3;
+    EditText subinfo4;
+    EditText subinfo5;
+    EditText description;
+    EditText title;
+    ImageButton AdImage1;
+    ImageButton AdImage2;
+    ImageButton AdImage3;
+    ImageButton AdImage4;
+    ImageButton AdImage5;
+
+    ProgressBar progressBar1;
+    ProgressBar progressBar2;
+    ProgressBar progressBar3;
+    ProgressBar progressBar4;
+    ProgressBar progressBar5;
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    int count =0;
+
+    String url= "http://rentitapi.herokuapp.com/ad_image";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +78,129 @@ public class PostAdActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        AdImage1 = (ImageButton)findViewById(R.id.AdImage1);
+        AdImage2 = (ImageButton)findViewById(R.id.AdImage2);
+        AdImage3 = (ImageButton)findViewById(R.id.AdImage3);
+        AdImage4 = (ImageButton)findViewById(R.id.AdImage4);
+        AdImage5 = (ImageButton)findViewById(R.id.AdImage5);
+
+
+        subinfo1 = (EditText)findViewById(R.id.subinfo1);
+        subinfo2 = (EditText)findViewById(R.id.subinfo2);
+        subinfo3 = (EditText)findViewById(R.id.subinfo3);
+        subinfo4 = (EditText)findViewById(R.id.subinfo4);
+        subinfo5 = (EditText)findViewById(R.id.subinfo5);
+        description = (EditText)findViewById(R.id.description);
+        title = (EditText)findViewById(R.id.title);
+
+        progressBar1= (ProgressBar)findViewById(R.id.progressBar1);
+        progressBar2= (ProgressBar)findViewById(R.id.progressBar2);
+        progressBar3= (ProgressBar)findViewById(R.id.progressBar3);
+        progressBar4= (ProgressBar)findViewById(R.id.progressBar4);
+        progressBar5= (ProgressBar)findViewById(R.id.progressBar5);
+
+        Spinner categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){//House
+                    subinfo3.setHint("Area");
+                    subinfo4.setHint("Bedrooms");
+                    subinfo5.setHint("Bathrooms");
+                }else if(position == 1){//car
+                    subinfo3.setHint("Brand");
+                    subinfo4.setHint("Model");
+                    subinfo5.setHint("Year of Manufacture");
+                }else if(position == 2){//heavy
+                    subinfo3.setHint("Number of seats");
+                    subinfo4.setHint("Number of wheels");
+                    subinfo5.setHint("Year of Manufacture");
+                }else if(position == 3){//Bikes
+                    subinfo3.setHint("Brand");
+                    subinfo4.setHint("Model");
+                    subinfo5.setHint("Year of Manufacture");
+                }else if(position == 4){//commercials
+                    subinfo3.setHint("Area");
+                    subinfo4.setHint("Parking Capacity");
+                    subinfo5.setHint("Year of Construction");
+                }else if(position == 5){//auditoriums
+                    subinfo3.setHint("Seating Capacity");
+                    subinfo4.setHint("Parking Capacity");
+                    subinfo5.setHint("Area");
+                }else if(position == 6){//sound
+                    subinfo3.setHint("Number of speakers");
+                    subinfo4.setHint("Power Specification");
+                    subinfo5.setVisibility(View.INVISIBLE);
+                }else if(position == 7){
+                    subinfo3.setHint("Type");
+                    subinfo4.setHint("Power Specification");
+                    subinfo5.setVisibility(View.INVISIBLE);
+                }else if(position == 8){
+                    subinfo3.setHint("Type");
+                    subinfo4.setVisibility(View.INVISIBLE);
+                    subinfo5.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        AdImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select picture"),1);
+
+
+            }
+        });
+        AdImage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select picture"),2);
+
+            }
+        });
+        AdImage3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select picture"),3);
+
+            }
+        });
+        AdImage4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select picture"),4);
+
+            }
+        });
+        AdImage5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select picture"),5);
+            }
+        });
     }
 
     @Override
@@ -102,4 +271,94 @@ public class PostAdActivity extends AppCompatActivity
         return true;
 
     }
+
+    Bitmap[] bitmap = new  Bitmap[6];
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode < 6 && requestCode >0 && resultCode == RESULT_OK && data!=null && data.getData()!=null){
+            Uri uri = data.getData();
+            try{
+                bitmap[requestCode-1] = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                if(requestCode == 1){
+                    AdImage1.setImageBitmap(bitmap[requestCode-1]);
+                    progressBar1.setVisibility(View.VISIBLE);
+                    progressBar1.setProgress(0);
+                }else if(requestCode == 2){
+                    AdImage2.setImageBitmap(bitmap[requestCode-1]);
+                    progressBar2.setVisibility(View.VISIBLE);
+                    progressBar2.setProgress(0);
+                }else if( requestCode == 3){
+                    AdImage3.setImageBitmap(bitmap[requestCode-1]);
+                    progressBar3.setVisibility(View.VISIBLE);
+                    progressBar3.setProgress(0);
+                }else if(requestCode == 4){
+                    AdImage4.setImageBitmap(bitmap[requestCode-1]);
+                    progressBar4.setVisibility(View.VISIBLE);
+                    progressBar4.setProgress(0);
+                }else if(requestCode == 5){
+                    AdImage5.setImageBitmap(bitmap[requestCode-1]);
+                    progressBar5.setVisibility(View.VISIBLE);
+                    progressBar5.setProgress(0);
+                }
+
+                SendHttpRequestTask t = new SendHttpRequestTask();
+                t.execute(requestCode);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class SendHttpRequestTask extends AsyncTask<Integer, Void, Integer> {
+
+
+        @Override
+        protected Integer doInBackground(Integer... params) {
+            Bitmap b = bitmap[params[0]-1];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            b.compress(Bitmap.CompressFormat.PNG, 0, baos);
+
+            try {
+                com.rentit.priyath.rentitlayout.HttpClient client = new com.rentit.priyath.rentitlayout.HttpClient(url);
+                client.connectForMultipart();
+                client.addFilePart("file", "priyath.png", baos.toByteArray());
+                client.finishMultipart();
+                String data = client.getResponse();
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(Integer requestCode) {
+            if(requestCode == 1){
+                AdImage1.setImageBitmap(bitmap[requestCode-1]);
+                progressBar1.setVisibility(View.INVISIBLE);
+                progressBar1.setProgress(0);
+            }else if(requestCode == 2){
+                AdImage2.setImageBitmap(bitmap[requestCode-1]);
+                progressBar2.setVisibility(View.INVISIBLE);
+                progressBar2.setProgress(0);
+            }else if( requestCode == 3){
+                AdImage3.setImageBitmap(bitmap[requestCode-1]);
+                progressBar3.setVisibility(View.INVISIBLE);
+                progressBar3.setProgress(0);
+            }else if(requestCode == 4){
+                AdImage4.setImageBitmap(bitmap[requestCode-1]);
+                progressBar4.setVisibility(View.INVISIBLE);
+                progressBar4.setProgress(0);
+            }else if(requestCode == 5){
+                AdImage5.setImageBitmap(bitmap[requestCode-1]);
+                progressBar5.setVisibility(View.INVISIBLE);
+                progressBar5.setProgress(0);
+            }
+
+        }
+    }
+
 }
